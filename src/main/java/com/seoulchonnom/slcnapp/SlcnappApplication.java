820f2c6ba13e743d9b7610649e197606aeedc5b6
SeoulChonnom <bk688035@gmail.com>
2024-11-23 00:44:54 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/seoulchonnom/slcnapp/SlcnappApplication.java
@@ -0,0 +1,13 @@
+package com.seoulchonnom.slcnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlcnappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlcnappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seoulchonnom/slcnapp/SlcnappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulchonnom.slcnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlcnappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
