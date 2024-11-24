6d05110706ea495f96851a990f51742be4c91d41
SeoulChonnom <bk688035@gmail.com>
2024-11-24 23:10:39 +0900

    feat: Trip 응답 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/trip/dto/QuizResponse.java
@@ -0,0 +1,20 @@
+package com.seoulchonnom.slcnapp.trip.dto;
+
+import com.seoulchonnom.slcnapp.trip.domain.Quiz;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuizResponse {
+	private String quizIndex;
+	private String answer;
+
+	public static QuizResponse from(Quiz quiz) {
+		return QuizResponse.builder()
+			.quizIndex(quiz.getQuizIndex())
+			.answer(quiz.getAnswer())
+			.build();
+	}
+}
---
+++ src/main/java/com/seoulchonnom/slcnapp/trip/dto/TripListResponse.java
@@ -0,0 +1,41 @@
+package com.seoulchonnom.slcnapp.trip.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.seoulchonnom.slcnapp.trip.domain.Quiz;
+import com.seoulchonnom.slcnapp.trip.domain.Trip;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TripListResponse {
+	private String info1;
+	private String info2;
+	private String logo;
+	private String quizTitle;
+	private String quizAnswer;
+	private String quizAnswerTitle;
+	private String quizAnswerText;
+	private String quizErrorTitle;
+	private String quizErrorText;
+	private List<QuizResponse> quizResponses;
+
+	public static TripListResponse from(Trip trip) {
+		return TripListResponse.builder()
+			.info1(trip.getInfo1())
+			.info2(trip.getInfo1())
+			.logo(trip.getLogo())
+			.quizTitle(trip.getQuizTitle())
+			.quizAnswer(trip.getQuizAnswer())
+			.quizAnswerTitle(trip.getQuizErrorTitle())
+			.quizAnswerText(trip.getQuizAnswerText())
+			.quizErrorTitle(trip.getQuizErrorTitle())
+			.quizErrorText(trip.getQuizErrorText())
+			.quizResponses(trip.getQuizList().stream().map(QuizResponse::from)
+				.collect(Collectors.toList()))
+			.build();
+	}
+}
