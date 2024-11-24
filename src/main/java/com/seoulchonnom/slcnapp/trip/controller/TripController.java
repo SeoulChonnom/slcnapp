93cd22c370e0fae0bac654475bab03bd9c311502
SeoulChonnom <bk688035@gmail.com>
2024-11-25 00:26:18 +0900

    feat: TripController 추가

--- src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
@@ -9,15 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seoulchonnom.slcnapp.common.dto.BaseResponse;
 import com.seoulchonnom.slcnapp.trip.service.TripService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/trip")
 @RequiredArgsConstructor
+@Tag(name = "나들이 정보 API", description = "나들이 상세정보 및 전체 정보 조회")
 public class TripController {
 	private final TripService tripService;
 
 	@GetMapping("/")
+	@Operation(summary = "전체 나들이 조회", description = "메인페이지 리스트 생성용 API")
 	public ResponseEntity<BaseResponse> getTrips() {
 		return new ResponseEntity<>(BaseResponse.from(true, "", tripService.getAllTripList()), HttpStatus.OK);
 	}
