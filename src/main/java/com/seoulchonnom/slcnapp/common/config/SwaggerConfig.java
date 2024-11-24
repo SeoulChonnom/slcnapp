be30fe76de5956934a2111cecaeca7fd7a0e10a9
SeoulChonnom <bk688035@gmail.com>
2024-11-25 00:36:40 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.seoulchonnom.slcnapp.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("SLCN API Test")
+			.description("SLCN API description")
+			.version("1.0.o");
+	}
+}
