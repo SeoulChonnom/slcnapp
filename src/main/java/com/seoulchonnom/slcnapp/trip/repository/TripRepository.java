ada867ffeb727eff3ec9b4acb358f3f133f4365d
SeoulChonnom <bk688035@gmail.com>
2024-11-24 21:39:11 +0900

    feat: TripRepository 생성

---
+++ src/main/java/com/seoulchonnom/slcnapp/trip/repository/TripRepository.java
@@ -0,0 +1,15 @@
+package com.seoulchonnom.slcnapp.trip.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import com.seoulchonnom.slcnapp.trip.domain.Trip;
+
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+	List<Trip> findAll();
+
+	Optional<Trip> findById(@NonNull Integer id);
+}
