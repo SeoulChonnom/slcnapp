bf85f7c7d7cb2f6a09c53173aaa833f64b16da78
SeoulChonnom <bk688035@gmail.com>
2024-11-25 23:56:21 +0900

    feat: TripController 상세정보 조회 메서드 추가

--- src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
@@ -1,13 +1,15 @@
 package com.seoulchonnom.slcnapp.trip.controller;
 
+import static com.seoulchonnom.slcnapp.trip.TripConstant.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seoulchonnom.slcnapp.common.dto.BaseResponse;
-import com.seoulchonnom.slcnapp.trip.TripConstant;
 import com.seoulchonnom.slcnapp.trip.service.TripService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,7 +27,15 @@ public class TripController {
 	@Operation(summary = "전체 나들이 조회", description = "메인페이지 리스트 생성용 API")
 	public ResponseEntity<BaseResponse> getTrips() {
 		return new ResponseEntity<>(
-			BaseResponse.from(true, TripConstant.RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE, tripService.getAllTripList()),
+			BaseResponse.from(true, RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE, tripService.getAllTripList()),
+			HttpStatus.OK);
+	}
+
+	@GetMapping("/{tripId}")
+	@Operation(summary = "나들이 상세정보 조회", description = "나들이 상세페이지용 API")
+	public ResponseEntity<BaseResponse> getTripById(@PathVariable("tripId") Integer tripId) {
+		return new ResponseEntity<>(
+			BaseResponse.from(true, RETRIEVE_TRIP_INFO_SUCCESS_MESSAGE, tripService.getTripById(tripId)),
 			HttpStatus.OK);
 	}
 }
