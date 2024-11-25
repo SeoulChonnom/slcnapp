502a937f21f6f272d8faf4b50f3951d911fb1d9d
SeoulChonnom <bk688035@gmail.com>
2024-11-25 23:54:42 +0900

    feat: TripConstant 변수 추가 및 수정

--- src/main/java/com/seoulchonnom/slcnapp/trip/TripConstant.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/TripConstant.java
@@ -7,5 +7,6 @@ import lombok.NoArgsConstructor;
 public class TripConstant {
 	public static final String TRIP_NOT_FOUND_ERROR_MESSAGE = "해당 일자 나들이가 없습니다.";
 
-	public static final String RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE = "나들이 조회에 성공하였습니다.";
+	public static final String RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE = "나들이 리스트 조회에 성공하였습니다.";
+	public static final String RETRIEVE_TRIP_INFO_SUCCESS_MESSAGE = "나들이 상세정보 조회에 성공하였습니다.";
 }
