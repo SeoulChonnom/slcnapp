fec41a57a9ad4d7982f1804a8a8fec633fd50291
SeoulChonnom <bk688035@gmail.com>
2024-12-13 23:11:27 +0900

    feat: UserDetail 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/user/domain/UserDetail.java
@@ -0,0 +1,61 @@
+package com.seoulchonnom.slcnapp.user.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDetail implements UserDetails {
+	private User user;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		user.getAuthorityList().forEach((a) -> authorities.add(() -> "ROLE_" + a));
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	// 아래의 옵션들로 Spring Security가 발생시킨다. 
+	// false일 경우 인증을 허용하지 않고 사용자는 권한을 얻지못해 엑세스를 못함
+	@Override
+	public boolean isAccountNonExpired() {
+		// 토큰 만료 확인하는 코드 필요
+		return true;
+	}
+
+	// 계정이 잠겨있는지 확인 / 사용자 직접 or 비밀번호 틀리면
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	// 비밀번호 유효기간 -> 변경 요청
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	// 계정 활성화 / 시큐리티는 사용자가 인증하도록 허용한다.
+	// false 일경우
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
