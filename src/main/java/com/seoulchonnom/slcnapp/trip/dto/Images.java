81b0b4d7bba50e1c57eb225ac8cfe1103fdf33b3
SeoulChonnom <bk688035@gmail.com>
2024-12-15 19:59:12 +0900

    feat: Images 생성

---
+++ src/main/java/com/seoulchonnom/slcnapp/trip/dto/Images.java
@@ -0,0 +1,13 @@
+package com.seoulchonnom.slcnapp.trip.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Images {
+    private String logo;
+    private String map1;
+    private String map2;
+}
