0fef76bdf7f49d39101f08a5adc1a5b15136e6c8
SeoulChonnom <bk688035@gmail.com>
2024-12-15 20:34:49 +0900

    feat: TripRegisterRequest 생성

---
+++ src/main/java/com/seoulchonnom/slcnapp/trip/dto/TripRegisterRequest.java
@@ -0,0 +1,32 @@
+package com.seoulchonnom.slcnapp.trip.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TripRegisterRequest {
+    private String date;
+    private String type;
+    private String info1;
+    private String info2;
+    private String logo;
+    private String map1;
+    private String map2;
+    private String button1;
+    private String button2;
+    private String drive;
+    private String quizTitle;
+    private String quizAnswer;
+    private String quizAnswerTitle;
+    private String quizAnswerText;
+    private String quizErrorTitle;
+    private String quizErrorText;
+    private List<QuizRegisterRequest> quizRegisterRequestList;
+}
