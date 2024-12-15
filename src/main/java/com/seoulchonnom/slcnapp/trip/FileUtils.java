a7fe44af3e18d0cc5a3db45c37108afac54cecad
SeoulChonnom <bk688035@gmail.com>
2024-12-15 20:27:18 +0900

    feat: fileUtils 추가 및 Images 제거

--- src/main/java/com/seoulchonnom/slcnapp/trip/FileUtils.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/FileUtils.java
@@ -1,16 +1,26 @@
 package com.seoulchonnom.slcnapp.trip;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 @Component
 public class FileUtils {
     @Value("${upload.path}")
     private String directory;
+
+    public String saveImages(MultipartFile multipartFile, String path) throws IOException {
+        String fileName = createSaveFileName(multipartFile.getOriginalFilename());
+        String saveFileName = directory + path + fileName;
+
+        multipartFile.transferTo(new File(saveFileName));
+
+        return saveFileName;
+    }
 
     // 파일 저장 이름 만들기
     // - 사용자들이 올리는 파일 이름이 같을 수 있으므로, 자체적으로 랜덤 이름을 만들어 사용한다
@@ -25,10 +35,4 @@ public class FileUtils {
         int pos = originalFilename.lastIndexOf(".");
         return originalFilename.substring(pos + 1);
     }
-
-    // fullPath 만들기
-    private String getFullPath(String filename) {
-        return directory + filename;
-    }
-
 }
