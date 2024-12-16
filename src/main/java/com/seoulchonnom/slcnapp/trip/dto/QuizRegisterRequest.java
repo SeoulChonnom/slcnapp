cc2ef87641810d0c882a975d339318312721ba8f
SeoulChonnom <bk688035@gmail.com>
2024-12-16 23:26:52 +0900

    feat: QuizRegisterRequest of 메서드 수정

--- src/main/java/com/seoulchonnom/slcnapp/trip/dto/QuizRegisterRequest.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/dto/QuizRegisterRequest.java
@@ -1,20 +1,22 @@
 package com.seoulchonnom.slcnapp.trip.dto;
 
 import com.seoulchonnom.slcnapp.trip.domain.Quiz;
-import lombok.*;
+import com.seoulchonnom.slcnapp.trip.domain.Trip;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuizRegisterRequest {
-    private String quizIndex;
-    private String answer;
+	private String quizIndex;
+	private String answer;
 
-    public Quiz of() {
-        return Quiz.builder()
-                .quizIndex(quizIndex)
-                .answer(answer)
-                .build();
-    }
+	public Quiz of(Trip trip) {
+		return Quiz.builder().quizIndex(quizIndex).trip(trip).answer(answer).build();
+	}
 }
