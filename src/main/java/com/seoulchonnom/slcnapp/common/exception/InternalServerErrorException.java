e58539ceea7d2efa327df768570493312a5c9c87
SeoulChonnom <bk688035@gmail.com>
2024-12-16 23:25:24 +0900

    feat: Exception 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/common/exception/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package com.seoulchonnom.slcnapp.common.exception;
+
+public class InternalServerErrorException extends RuntimeException {
+	public InternalServerErrorException(String message) {
+		super(message);
+	}
+}
