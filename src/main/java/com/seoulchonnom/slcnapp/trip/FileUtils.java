5dd94a8528eaf749965e5e21ec5afb8d17ad6c7f
SeoulChonnom <bk688035@gmail.com>
2024-12-16 23:26:29 +0900

    feat: FileUtils 추가

--- src/main/java/com/seoulchonnom/slcnapp/trip/FileUtils.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/FileUtils.java
@@ -1,38 +1,61 @@
 package com.seoulchonnom.slcnapp.trip;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
+import static com.seoulchonnom.slcnapp.trip.TripConstant.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.seoulchonnom.slcnapp.trip.exception.TripFileExtException;
+import com.seoulchonnom.slcnapp.trip.exception.TripFileSizeException;
+
 @Component
 public class FileUtils {
-    @Value("${upload.path}")
-    private String directory;
+	@Value("${upload.path}")
+	private String directory;
 
-    public String saveImages(MultipartFile multipartFile, String path) throws IOException {
-        String fileName = createSaveFileName(multipartFile.getOriginalFilename());
-        String saveFileName = directory + path + fileName;
+	public String saveImages(MultipartFile multipartFile, String path) throws IOException {
 
-        multipartFile.transferTo(new File(saveFileName));
+		if (multipartFile.getSize() > MAX_FILE_SIZE) {
+			throw new TripFileSizeException();
+		}
 
-        return saveFileName;
-    }
+		if (!isImage(multipartFile.getOriginalFilename())) {
+			throw new TripFileExtException();
+		}
 
-    // 파일 저장 이름 만들기
-    // - 사용자들이 올리는 파일 이름이 같을 수 있으므로, 자체적으로 랜덤 이름을 만들어 사용한다
-    private String createSaveFileName(String originalFilename) {
-        String ext = extractExt(originalFilename);
-        String uuid = UUID.randomUUID().toString();
-        return uuid + "." + ext;
-    }
+		String fileName = createSaveFileName(multipartFile.getOriginalFilename());
+		String saveFileName = directory + path + fileName;
 
-    // 확장자명 구하기
-    private String extractExt(String originalFilename) {
-        int pos = originalFilename.lastIndexOf(".");
-        return originalFilename.substring(pos + 1);
-    }
+		multipartFile.transferTo(new File(saveFileName));
+
+		return saveFileName;
+	}
+
+	// 파일 저장 이름 만들기
+	// - 사용자들이 올리는 파일 이름이 같을 수 있으므로, 자체적으로 랜덤 이름을 만들어 사용한다
+	private String createSaveFileName(String originalFilename) {
+		String ext = extractExt(originalFilename);
+		String uuid = UUID.randomUUID().toString();
+		return uuid + "." + ext;
+	}
+
+	// 확장자명 구하기
+	private String extractExt(String originalFilename) {
+		int pos = originalFilename.lastIndexOf(".");
+		return originalFilename.substring(pos + 1);
+	}
+
+	private boolean isImage(String originalFilename) {
+
+		if (extractExt(originalFilename).matches(EXT_REGEX_STRING)) {
+			return true;
+		}
+		return false;
+
+	}
 }
