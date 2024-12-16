e0f0166c8842204502bc69c7a9533361716aee8d
SeoulChonnom <bk688035@gmail.com>
2024-12-16 23:28:10 +0900

    feat: TripController 파일 관련 메서드 추가

--- src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
+++ src/main/java/com/seoulchonnom/slcnapp/trip/controller/TripController.java
@@ -1,17 +1,29 @@
 package com.seoulchonnom.slcnapp.trip.controller;
 
+import static com.seoulchonnom.slcnapp.trip.TripConstant.*;
+
+import java.nio.file.Paths;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.seoulchonnom.slcnapp.common.dto.BaseResponse;
+import com.seoulchonnom.slcnapp.trip.dto.ImageFile;
 import com.seoulchonnom.slcnapp.trip.dto.TripRegisterRequest;
 import com.seoulchonnom.slcnapp.trip.service.TripService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import static com.seoulchonnom.slcnapp.trip.TripConstant.*;
 
 @RestController
 @RequestMapping("/trip")
@@ -24,8 +36,7 @@ public class TripController {
 	@Operation(summary = "전체 나들이 조회", description = "메인페이지 리스트 생성용 API")
 	public ResponseEntity<BaseResponse> getTrips() {
 		return new ResponseEntity<>(
-			BaseResponse.from(true, RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE, tripService.getAllTripList()),
-			HttpStatus.OK);
+			BaseResponse.from(true, RETRIEVE_TRIP_LIST_SUCCESS_MESSAGE, tripService.getAllTripList()), HttpStatus.OK);
 	}
 
 	@GetMapping("/{tripDate}")
@@ -36,12 +47,23 @@ public class TripController {
 			HttpStatus.OK);
 	}
 
-	@PostMapping("/")
+	@PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@Operation(summary = "나들이 추가", description = "나들이 추가용 API")
-	public ResponseEntity<BaseResponse> createTrip(@RequestParam TripRegisterRequest tripRegisterRequest, @RequestPart MultipartFile logo, @RequestPart MultipartFile map1, @RequestPart MultipartFile map2) {
-		return new ResponseEntity<>(
-				BaseResponse.from(true, REGISTER_TRIP_SUCCESS_MESSAGE, tripService.registerTrip(tripRegisterRequest, logo, map1, map2)),
-				HttpStatus.OK
-		);
+	public ResponseEntity<BaseResponse> createTrip(
+		@RequestPart(value = "tripRegisterRequest") TripRegisterRequest tripRegisterRequest,
+		@RequestPart(value = "logo") MultipartFile logo, @RequestPart(value = "map1") MultipartFile map1,
+		@RequestPart(required = false, value = "map2") MultipartFile map2) {
+		return new ResponseEntity<>(BaseResponse.from(true, REGISTER_TRIP_SUCCESS_MESSAGE,
+			tripService.registerTrip(tripRegisterRequest, logo, map1, map2)), HttpStatus.OK);
+	}
+
+	@GetMapping("/file")
+	@Operation(summary = "이미지 조회", description = "이미지 조회용 API")
+	public ResponseEntity<byte[]> getFile(@RequestParam(value = "path") String path) {
+		ImageFile imageFile = tripService.getImageFile(Paths.get(path));
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.contentType(MediaType.valueOf(imageFile.getMimeType()))
+			.body(imageFile.getImage());
 	}
 }
