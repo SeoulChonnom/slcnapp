265eb49197d96a6891095b87dd556163d7e477de
SeoulChonnom <bk688035@gmail.com>
2024-12-18 14:28:37 +0900

    feat: SpringSecurity 컨피그 수정

--- src/main/java/com/seoulchonnom/slcnapp/common/config/SecurityConfiguration.java
+++ src/main/java/com/seoulchonnom/slcnapp/common/config/SecurityConfiguration.java
@@ -5,23 +5,43 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+
+import com.seoulchonnom.slcnapp.common.entrypoint.CommonAuthenticationEntryPoint;
+import com.seoulchonnom.slcnapp.common.filter.JwtAuthenticationFilter;
+import com.seoulchonnom.slcnapp.common.handler.CommonAccessDeniedHandler;
+import com.seoulchonnom.slcnapp.user.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// TODO: 보안 로직 수정 필요
 		http.csrf(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests((authorizeRequests) ->
-				authorizeRequests
-					.requestMatchers("/", "/swagger-ui/**").permitAll()
-					.requestMatchers("/trip/").permitAll()
-					.anyRequest().permitAll());
+			.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+				.requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+				.requestMatchers("/user/login", "/user/token").permitAll()
+				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+				.requestMatchers("/user/register").hasAuthority("ADMIN")
+				.anyRequest().hasAuthority("USER"))
+			.exceptionHandling(handling -> handling.authenticationEntryPoint(new CommonAuthenticationEntryPoint()))
+			.exceptionHandling(handling -> handling.accessDeniedHandler(new CommonAccessDeniedHandler()))
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+				UsernamePasswordAuthenticationFilter.class);
+		;
 		return http.build();
 	}
 
