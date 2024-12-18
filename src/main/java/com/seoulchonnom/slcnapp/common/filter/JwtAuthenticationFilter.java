b7ecfb407afaca032e44eb07675b27c4625a8ee9
SeoulChonnom <bk688035@gmail.com>
2024-12-18 14:28:17 +0900

    feat: JWT 인증 필터 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/common/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.seoulchonnom.slcnapp.common.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.seoulchonnom.slcnapp.user.JwtTokenProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+		ServletException {
+		String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+		if (token != null && jwtTokenProvider.validateToken(token)) {
+			Authentication authentication = jwtTokenProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		chain.doFilter(request, response);
+	}
+}
