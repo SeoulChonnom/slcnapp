d34b9918477eb87735cf6ad7e4af3b7014a495fd
SeoulChonnom <bk688035@gmail.com>
2024-12-18 14:23:46 +0900

    feat: UserConstant 추가

--- src/main/java/com/seoulchonnom/slcnapp/user/UserConstant.java
+++ src/main/java/com/seoulchonnom/slcnapp/user/UserConstant.java
@@ -5,9 +5,15 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserConstant {
-	public static final String INVALID_USER_LOGIN_REQUEST_MESSAGE = "잘못된 아이디 혹은 비밀번호 입니다..";
+	public static final int COOKIE_EXPIRE_TIME = 60 * 60 * 24 * 14;
+
+	public static final String USERNAME_NOT_FOUND_ERROR_MESSAGE = "해당하는 ID의 유저가 없습니다.";
 
 	public static final String USER_REGISTER_SUCCESS_MESSAGE = "회원가입에 성공하였습니다.";
 	public static final String USER_LOGIN_SUCCESS_MESSAGE = "로그인에 성공하였습니다.";
 
+	public static final String ACCESS_TOKEN_INVALID_ERROR_MESSAGE = "인증 정보가 잘못 되었습니다.";
+	public static final String ACCESS_ROLE_MISSING_ERROR_MESSAGE = "접근 권한이 없습니다.";
+	public static final String ACCESS_ROLE_DENIED_ERROR_MESSAGE = "접근 권한이 부족합니다.";
+	public static final String REFRESH_TOKEN_INVALID_ERROR_MESSAGE = "리프레쉬 토큰이 유효하지 않습니다.";
 }
