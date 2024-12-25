221e8e1ebdc3d87847c8808879af28d58beb0dcc
SeoulChonnom <bk688035@gmail.com>
2024-12-25 20:22:29 +0900

    feat: RedisConfig 추가

---
+++ src/main/java/com/seoulchonnom/slcnapp/common/config/RedisConfig.java
@@ -0,0 +1,62 @@
+package com.seoulchonnom.slcnapp.common.config;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	@Value("${spring.data.redis.password}")
+	private String redisPassword;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+		redisConfig.setPassword(redisPassword);
+
+		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+			.commandTimeout(Duration.ofSeconds(1)) //No longer than 1 second
+			.shutdownTimeout(Duration.ZERO) //Immediately shutdown after application shutdown
+			.build();
+
+		log.info("Connected to Redis at {}:{}", host, port);
+		return new LettuceConnectionFactory(redisConfig, clientConfig);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+		// 일반적인 key:value의 경우 시리얼라이저
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+		// Hash를 사용할 경우 시리얼라이저
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+		// 모든 경우
+		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+		return redisTemplate;
+	}
+}
