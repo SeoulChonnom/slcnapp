ec7533d9517fce53d7fe1a694f5678679d91aeb9
SeoulChonnom <bk688035@gmail.com>
2025-03-13 10:11:54 +0900

    feat: ErrorResponse 추가 및 입력 오류 핸들러 추가

--- src/main/java/com/seoulchonnom/slcnapp/common/handler/CommonExceptionHandler.java
+++ src/main/java/com/seoulchonnom/slcnapp/common/handler/CommonExceptionHandler.java
@@ -1,43 +1,51 @@
 package com.seoulchonnom.slcnapp.common.handler;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.seoulchonnom.slcnapp.common.dto.BaseResponse;
+import com.seoulchonnom.slcnapp.common.dto.ErrorResponse;
 import com.seoulchonnom.slcnapp.common.exception.BadRequestException;
 import com.seoulchonnom.slcnapp.common.exception.InternalServerErrorException;
 import com.seoulchonnom.slcnapp.common.exception.PayloadTooLargeException;
 import com.seoulchonnom.slcnapp.common.exception.UnsupportedMediaTypeException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @RestControllerAdvice
 public class CommonExceptionHandler {
 	@ExceptionHandler(BadRequestException.class)
-	public ResponseEntity<BaseResponse> badRequest(BadRequestException e) {
+	public ResponseEntity<ErrorResponse> badRequest(BadRequestException e) {
 		return new ResponseEntity<>(
-			BaseResponse.from(false, e.getMessage()),
+			ErrorResponse.from(false, e.getMessage()),
 			HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(InternalServerErrorException.class)
-	public ResponseEntity<BaseResponse> internalServerErrorException(InternalServerErrorException e) {
+	public ResponseEntity<ErrorResponse> internalServerErrorException(InternalServerErrorException e) {
 		return new ResponseEntity<>(
-			BaseResponse.from(false, e.getMessage()),
+			ErrorResponse.from(false, e.getMessage()),
 			HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(PayloadTooLargeException.class)
-	public ResponseEntity<BaseResponse> payloadTooLargeException(PayloadTooLargeException e) {
+	public ResponseEntity<ErrorResponse> payloadTooLargeException(PayloadTooLargeException e) {
 		return new ResponseEntity<>(
-			BaseResponse.from(false, e.getMessage()),
+			ErrorResponse.from(false, e.getMessage()),
 			HttpStatus.PAYLOAD_TOO_LARGE);
 	}
 
 	@ExceptionHandler(UnsupportedMediaTypeException.class)
-	public ResponseEntity<BaseResponse> unsupportedMediaTypeException(UnsupportedMediaTypeException e) {
+	public ResponseEntity<ErrorResponse> unsupportedMediaTypeException(UnsupportedMediaTypeException e) {
 		return new ResponseEntity<>(
-			BaseResponse.from(false, e.getMessage()),
+			ErrorResponse.from(false, e.getMessage()),
 			HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ErrorResponse> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+		return new ResponseEntity<>(
+			ErrorResponse.from(false, "입력이 올바르지 않습니다."),
+			HttpStatus.BAD_REQUEST);
 	}
 }
