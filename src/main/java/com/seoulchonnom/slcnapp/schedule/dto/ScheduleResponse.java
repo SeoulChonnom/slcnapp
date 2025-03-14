89f74f18ec3edbbb1c75b691e51f040c8c9a0101
SeoulChonnom <bk688035@gmail.com>
2025-03-14 15:43:01 +0900

    fix: ScheduleResponse 직렬화 수정

--- src/main/java/com/seoulchonnom/slcnapp/schedule/dto/ScheduleResponse.java
+++ src/main/java/com/seoulchonnom/slcnapp/schedule/dto/ScheduleResponse.java
@@ -1,5 +1,9 @@
 package com.seoulchonnom.slcnapp.schedule.dto;
 
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.seoulchonnom.slcnapp.schedule.domain.Schedule;
 import com.seoulchonnom.slcnapp.schedule.domain.ScheduleCategory;
 import com.seoulchonnom.slcnapp.schedule.domain.ScheduleState;
@@ -14,7 +18,10 @@ public class ScheduleResponse {
 	private String calendarId;
 	private String title;
 	private String body;
+
+	@JsonProperty(value="isAllday")
 	private boolean isAllDay;
+
 	private String start;
 	private String end;
 	private long goingDuration;
@@ -24,16 +31,53 @@ public class ScheduleResponse {
 	private String dueDateClass;
 	private String recurrenceRule;
 	private ScheduleState state;
+
+	@JsonProperty(value="isVisible")
 	private boolean isVisible;
+	@JsonProperty(value="isPending")
 	private boolean isPending;
+	@JsonProperty(value="isFocused")
 	private boolean isFocused;
+	@JsonProperty(value="isReadOnly")
 	private boolean isReadOnly;
+	@JsonProperty(value="isPrivate")
 	private boolean isPrivate;
+
 	private String color;
 	private String backgroundColor;
 	private String dragBackgroundColor;
 	private String borderColor;
 	private String customStyle;
+
+	@JsonIgnore
+	public boolean isAllDay() {
+		return isAllDay;
+	}
+
+	@JsonIgnore
+	public boolean isVisible() {
+		return isVisible;
+	}
+
+	@JsonIgnore
+	public boolean isPending() {
+		return isPending;
+	}
+
+	@JsonIgnore
+	public boolean isFocused() {
+		return isFocused;
+	}
+
+	@JsonIgnore
+	public boolean isReadOnly() {
+		return isReadOnly;
+	}
+
+	@JsonIgnore
+	public boolean isPrivate() {
+		return isPrivate;
+	}
 
 	public static ScheduleResponse from(Schedule schedule) {
 		return ScheduleResponse.builder()
@@ -42,8 +86,8 @@ public class ScheduleResponse {
 			.title(schedule.getTitle())
 			.body(schedule.getBody())
 			.isAllDay(schedule.isAllDay())
-			.start(schedule.getStart().toString())
-			.end(schedule.getEnd().toString())
+			.start(schedule.getStart().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+			.end(schedule.getEnd().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
 			.goingDuration(schedule.getGoingDuration())
 			.comingDuration(schedule.getComingDuration())
 			.location(schedule.getLocation())
