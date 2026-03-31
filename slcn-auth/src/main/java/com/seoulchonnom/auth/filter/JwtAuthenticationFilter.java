2a4ae00620cb7abdc8cb8433223ce68fc0e62450
SeoulChonnom <bk688035@gmail.com>
2026-03-31 18:43:42 +0900

    fix: filter Public URL 수정

--- slcn-auth/src/main/java/com/seoulchonnom/auth/filter/JwtAuthenticationFilter.java
+++ slcn-auth/src/main/java/com/seoulchonnom/auth/filter/JwtAuthenticationFilter.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-	private static final Set<String> PUBLIC_AUTH_PATHS = Set.of("/user/login", "/user/register", "/user/token");
+	private static final Set<String> PUBLIC_AUTH_PATHS = Set.of("/user/login", "/user/token");
 
 	private final JwtTokenProvider jwtTokenProvider;
 
