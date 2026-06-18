98281f2b53c56c0a72f3c7a13913077c72f6ed4d
SeoulChonnom <bk688035@gmail.com>
2026-06-18 18:03:00 +0900

    fix: 정규표현식 수정

--- slcn-spec/src/main/java/com/seoulchonnom/spec/file/constant/FileConstant.java
+++ slcn-spec/src/main/java/com/seoulchonnom/spec/file/constant/FileConstant.java
@@ -9,7 +9,7 @@ public class FileConstant {
 
 	public static final String AVAILABLE_PATH = "logo|map";
 	public static final String EXT_REGEX_STRING = "jpg|png|jpeg|gif|svg";
-	public static final String FILE_PATH_REGEX_STRING = "/(logo|map)\\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}.(jpg|png|jpeg|gif|svg)$";
+	public static final String FILE_PATH_REGEX_STRING = "(logo|map)/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}\\.(jpg|png|jpeg|gif|svg)";
 
 	public static final String RETRIEVE_FILE_SUCCESS_MESSAGE = "파일 조회에 성공하였습니다.";
 	public static final String FILE_UPLOAD_SUCCESS_MESSAGE = "파일 업로드에 성공하였습니다.";
