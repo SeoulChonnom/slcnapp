3443b570f0cc53b8ead6200b51d8adfaf1b6f94d
SeoulChonnom <bk688035@gmail.com>
2026-06-26 11:26:43 +0900

    fix: 쿠키 기능 추가

--- slcn-boot/src/main/java/com/seoulchonnom/boot/common/config/WebConfig.java
+++ slcn-boot/src/main/java/com/seoulchonnom/boot/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.seoulchonnom.boot.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	@Value("${slcn.domain:http://localhost:9090}")
+	private String domain;
+
 	//  Interceptor를 이용해서 처리하므로 전역의 Cross Origin 처리를 해준다.
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns(
+				"http://localhost:5173",
+				"http://localhost:8080",
+				domain
+			)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowCredentials(true)
 			.maxAge(6000);
 	}
 }
