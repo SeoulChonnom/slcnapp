8f3b7bb8b16d1cede7f951fedd12729484273f74
SeoulChonnom <bk688035@gmail.com>
2026-07-26 14:39:47 +0900

    refactor: http method 변경

--- slcn-rest/src/main/java/com/seoulchonnom/rest/schedule/ScheduleResource.java
+++ slcn-rest/src/main/java/com/seoulchonnom/rest/schedule/ScheduleResource.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -55,7 +54,7 @@ public class ScheduleResource implements ScheduleFacade {
 	}
 
 	@Override
-	@PatchMapping("/{scheduleId}/hide")
+	@PutMapping("/{scheduleId}/hide")
 	public ResponseEntity<Void> hideSchedule(@PathVariable("scheduleId") String scheduleId) {
 		scheduleLogic.hideSchedule(scheduleId);
 		return ResponseEntity.noContent().build();
--- slcn-rest/src/main/java/com/seoulchonnom/rest/travel/TravelResource.java
+++ slcn-rest/src/main/java/com/seoulchonnom/rest/travel/TravelResource.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seoulchonnom.aggregate.travel.logic.TravelLogic;
@@ -47,7 +47,7 @@ public class TravelResource implements TravelFacade {
 	}
 
 	@Override
-	@RequestMapping(value = "/{travelId}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+	@PutMapping("/{travelId}")
 	public ResponseEntity<TravelDetailRdo> modifyTravel(@PathVariable("travelId") String travelId,
 		@RequestBody TravelUdo travelUdo) {
 		return new ResponseEntity<>(travelLogic.modifyTravel(travelId, travelUdo), HttpStatus.OK);
